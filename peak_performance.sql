a12a3c92280038df1f33c8b091df43ed0bc7f556
Pasha <pasha182006@gmail.com>
2026-02-25 23:14:22 +0300

    обновления бд

--- peak_performance.sql
+++ peak_performance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: MySQL-8.4
--- Время создания: Фев 19 2026 г., 19:59
+-- Время создания: Фев 25 2026 г., 23:13
 -- Версия сервера: 8.4.6
 -- Версия PHP: 8.4.12
 
@@ -78,6 +78,7 @@ CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('user','admin') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user',
   `phone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -89,10 +90,11 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `phone`, `password`, `created_at`, `reset_token`, `reset_expires`) VALUES
-(1, 'PASHA', 'pasha182006@gmail.com', '79288844420', '$2y$12$KV5Cva2jh8a93DqouFfBuuGADx2G8jA5zT7cgEf0/rbcjJ.3Sq9RC', '2025-12-09 20:41:26', 'd666c8025b5fa74260d190407ad0598d50bddadde287b50f8413c4ffe30b44a2', '2026-01-28 15:40:43'),
-(4, 'killer123', 'killer123@gmail.com', '79284990033', '$2y$12$GwfYDk3qxoRzOGi7M9Rv8.WDXF2Bk7gtfP38O5cetjYd9zFstCRTW', '2025-12-11 15:27:51', NULL, NULL),
-(5, 'killer1233', 'killer1233@gmail.com', '79284990033', '$2y$12$7uQGJXpFZqF0n1DcSCK0Y.7gvBwjP59hIWuNU.ZoB47yVYMCvBQpy', '2025-12-11 15:30:04', NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `email`, `role`, `phone`, `password`, `created_at`, `reset_token`, `reset_expires`) VALUES
+(1, 'PASHA', 'pasha182006@gmail.com', 'user', '79288844420', '$2y$12$KV5Cva2jh8a93DqouFfBuuGADx2G8jA5zT7cgEf0/rbcjJ.3Sq9RC', '2025-12-09 20:41:26', 'd666c8025b5fa74260d190407ad0598d50bddadde287b50f8413c4ffe30b44a2', '2026-01-28 15:40:43'),
+(4, 'killer123', 'killer123@gmail.com', 'user', '79284990033', '$2y$12$GwfYDk3qxoRzOGi7M9Rv8.WDXF2Bk7gtfP38O5cetjYd9zFstCRTW', '2025-12-11 15:27:51', NULL, NULL),
+(5, 'killer1233', 'killer1233@gmail.com', 'user', '79284990033', '$2y$12$7uQGJXpFZqF0n1DcSCK0Y.7gvBwjP59hIWuNU.ZoB47yVYMCvBQpy', '2025-12-11 15:30:04', NULL, NULL),
+(12, 'admin', 'admin@peak.local', 'admin', '+79991234567', '$2y$12$Y7mCBzY3EG8Wdo3fyPumku/Mj1iKmSHxNpHR0wxwt42wxMxDf984K', '2026-02-25 11:04:51', NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -134,13 +136,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
 
 --
 -- AUTO_INCREMENT для таблицы `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
@@ -152,7 +154,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
